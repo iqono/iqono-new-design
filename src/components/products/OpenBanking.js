02782c3de22dd893899b73f78e1024c5a4451cb7
iqono <it@iqono.com>
2025-10-14 01:26:06 +0300

    Update OpenBanking.js

--- src/components/products/OpenBanking.js
+++ src/components/products/OpenBanking.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../Button';
 import PromoSection from '../PromoSection';
-import FAQ from '../FAQ';
+import FAQbanking from '../FAQbanking';
 import { Link } from 'react-router-dom';
 
 function OpenBanking() {
@@ -57,7 +57,7 @@ function OpenBanking() {
           </div>
         </div>
         <div className='hidden md:block' data-aos="fade-up" data-aos-delay="300">
-          <FAQ pageType="product" />
+          <FAQbanking pageType="product" />
         </div>
       </div>
       <div data-aos="fade-up" data-aos-delay="100">
