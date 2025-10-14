b7e7d237d0d65048687c4e0789841923c9170113
iqono <it@iqono.com>
2025-10-14 11:58:39 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -233,10 +233,10 @@ const Home = () => {
           <div className='flex justify-between flex-col md:flex-row'>
             <div className='max-w-[870px] w-full md:w-1/2' data-aos="fade-right" data-aos-delay="100">
               <p className='text-5xl leading-[1.1] text-white'>
-                How Get started?
+                Your Journey with iQono
               </p>
               <p className='text-base leading-[1.1] text-white mt-10 opacity-80'>
-                From Sign-Up to Scaling — Seamlessly.
+                Building Success — Step by Step
               </p>
               <img src='/get-started.png' className='w-[80%]' />
             </div>
